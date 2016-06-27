43c614ebf4dc319ba89535ae3125d0cebe1154a0
franco <francojohnc>
2016-06-27 14:52:32 +0800

    open folder not working yet

--- app/src/main/java/apkmarvel/filebrowser/MainActivity.java
+++ app/src/main/java/apkmarvel/filebrowser/MainActivity.java
@@ -1,7 +1,9 @@
 package apkmarvel.filebrowser;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -9,25 +11,34 @@ import android.webkit.MimeTypeMap;
 
 public class MainActivity extends AppCompatActivity {
     private static final int OPEN_FILE_REQUEST_CODE = 1;
+    private static final int OPEN_FOLDER_REQUEST_CODE = 2;
     public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void open(View v){
+    public void OpenFile(View v){
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_GET_CONTENT);
         intent.setType("*/*");
         startActivityForResult(intent, OPEN_FILE_REQUEST_CODE);
     }
+    public void OpenFolder(View v){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getPath()
+                + "/myFolder/");
+        intent.setDataAndType(uri, "text/csv");
+        startActivity(Intent.createChooser(intent, "Open folder"));
+
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.e(TAG, "requestCode: "+requestCode);
-        Log.e(TAG, "resultCode: "+resultCode);
-        Log.e(TAG, "Intent: "+data.toString());
-        Log.e(TAG, "getData Uri: "+data.getData().toString());
         if(requestCode == OPEN_FILE_REQUEST_CODE && resultCode == RESULT_OK) {
+            Log.e(TAG, "requestCode: "+requestCode);
+            Log.e(TAG, "resultCode: "+resultCode);
+            Log.e(TAG, "Intent: "+data.toString());
+            Log.e(TAG, "getData Uri: "+data.getData().toString());
             String extension = MimeTypeMap.getSingleton().getExtensionFromMimeType(getContentResolver().getType(data.getData()));
             if (extension == null) {
                 Log.e(TAG, "File has no extension or invalid extension");
