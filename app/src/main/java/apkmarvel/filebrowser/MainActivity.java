5adf4b233acc4993e9cf71fbc8ad8efa718a5781
franco <francojohnc>
2016-06-27 16:23:37 +0800

    file browser

--- app/src/main/java/apkmarvel/filebrowser/MainActivity.java
+++ app/src/main/java/apkmarvel/filebrowser/MainActivity.java
@@ -9,6 +9,9 @@ import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private static final int OPEN_FILE_REQUEST_CODE = 1;
     private static final int OPEN_FOLDER_REQUEST_CODE = 2;
@@ -17,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        File root = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
+        getfile(root);
+        for (int i = 0; i < fileList.size(); i++) {
+            Log.e(TAG, "file: "+fileList.get(i).getName());
+            if (fileList.get(i).isDirectory()) {
+            }
+        }
     }
     public void OpenFile(View v){
         Intent intent = new Intent();
@@ -39,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, "resultCode: "+resultCode);
             Log.e(TAG, "Intent: "+data.toString());
             Log.e(TAG, "getData Uri: "+data.getData().toString());
+            Log.e(TAG, "getPath : "+data.getData().getPath());
             String extension = MimeTypeMap.getSingleton().getExtensionFromMimeType(getContentResolver().getType(data.getData()));
             if (extension == null) {
                 Log.e(TAG, "File has no extension or invalid extension");
@@ -47,5 +58,27 @@ public class MainActivity extends AppCompatActivity {
                 Log.e(TAG, "File extension: " + extension);
             }
         }
+    }
+    private ArrayList<File> fileList = new ArrayList<>();
+    public ArrayList<File> getfile(File dir) {
+        File listFile[] = dir.listFiles();
+        if (listFile != null && listFile.length > 0) {
+            for (int i = 0; i < listFile.length; i++) {
+                if (listFile[i].isDirectory()) {
+                    fileList.add(listFile[i]);
+                    getfile(listFile[i]);
+                } else {
+                    if (listFile[i].getName().endsWith(".png")
+                            || listFile[i].getName().endsWith(".jpg")
+                            || listFile[i].getName().endsWith(".jpeg")
+                            || listFile[i].getName().endsWith(".gif"))
+                    {
+                        fileList.add(listFile[i]);
+                    }
+                }
+
+            }
+        }
+        return fileList;
     }
 }
